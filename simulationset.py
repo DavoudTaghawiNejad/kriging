719395cc79be709d0b1b04fc09af0f603cd0ead3
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-06-26 15:03:35 +0000

    static setup and best

--- simulationset.py
+++ simulationset.py
@@ -1,18 +1,35 @@
 __author__ = 'taghawi'
-class SimulationSet:
-    def __init__(self, input_keys, target):
-        self.input_keys = input_keys
-        self.target = self.encode(target)
-        self.input = []
-        self.result = []
+from commonset import get_two_hierarchies_of_keys
+import numpy as np
 
-    def encode(self, what):
-        return [what[category][key] for category, key in self.keys]
+class SimulationSet:
+    @staticmethod
+    def setup(dtypes, target):
+        SimulationSet.input_keys = get_two_hierarchies_of_keys(dtypes)
+        SimulationSet.output_keys = get_two_hierarchies_of_keys(target)
+        SimulationSet.target = encode(target, SimulationSet.output_keys)
+
+    def __init__(self):
+        self.inputs = []
+        self.results = []
 
     def insert(self, input, output):
-        self.input.append(self.encode(input))
-        self.result.append(self.metric(output))
+        self.inputs.append(encode(input, SimulationSet.input_keys))
+        self.results.append(self.metric(output))
 
     def metric(self, output):
-        output = self.encode(output)
-        return sum(((self.target - output) / self.target) ** 2)
+        output = encode(output, SimulationSet.output_keys)
+        return sum(((SimulationSet.target - output) / SimulationSet.target) ** 2)
+
+    def best(self):
+        index = np.argmin(self.results)
+        input = self.inputs[index]
+        result = self.results[index]
+        assert not(np.isnan(input).any())
+        return input, result
+
+    def __len__(self):
+        return len(self.inputs)
+
+def encode(what, keys):
+    return [what[category][key] for category, key in keys]
