2d0e0e9e09a4b106cdecdaa545252229dd0480ae
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-07-25 11:29:26 -0400

    BUGFIX: lowest_values.pyx

--- kriging_model.py
+++ kriging_model.py
@@ -7,18 +7,16 @@ import logging
 logger = logging.getLogger('kriging.kriger')
 
 
-def kriger(simulations, InputSet, schlinge, num_return_best, gp):
+def kriger(simulations, InputSet, schlinge, num_return_best, old_best_metric, gp):
     gp.fit(simulations.inputs, simulations.metrics)
     best_input, best_result = simulations.best()
     t = time()
-    inputs, metrics, negative_values = lowest_values(num_return_best, len(InputSet.lb), gp.predict, simulations, InputSet, best_input, schlinge)
+    inputs, metrics, negative_values = lowest_values(num_return_best, len(InputSet.lb), gp.predict, simulations, InputSet, best_input, schlinge, old_best_metric)
     logger.info("    time: %f" % (time() - t))
     logger.info("    candidates better: %i" % len(inputs))
     logger.info("    candidates < 0: %i" % negative_values)
     candidates = PredictedSet()
     for inputs, metrics in zip(inputs, metrics):
         candidates.insert(inputs, metrics)
-    if negative_values:
-        logger.info("/// %i negative values" % negative_values)
     return candidates
 
--- lowest_values.pyx
+++ lowest_values.pyx
@@ -3,11 +3,11 @@ import itertools
 import numpy as np
 from hypercube import centered_latin_hypercube_A
 
-def lowest_values(n, ncparm, predict, simulations, InputSet, best_input, schlinge):
+def lowest_values(n, ncparm, predict, simulations, InputSet, best_input, schlinge, best_metric):
     """ uses predict to predict the outcome of all parameters in chains, return the 20 most promising values
     """
     cdef int negative_value = 0
-    cdef float nth = float("Inf")
+    cdef float nth = best_metric
     cdef float y
     candidate_ys = np.ones(n, float) * float("Inf")
     candidate_xs = np.ones((n, ncparm), float) * float("Inf")
@@ -15,11 +15,11 @@ def lowest_values(n, ncparm, predict, simulations, InputSet, best_input, schling
         for x in centered_latin_hypercube_A(best_input, schlinge, InputSet.lb, InputSet.ub, ncparm * 10):
             if simulations.is_new(x):
                 y = predict(x)
-                if 0 > y:
+                if y < 0:
                     negative_value += 1
                 elif y <= nth:
                     i = np.argmax(candidate_ys)
                     candidate_ys[i] = y
                     candidate_xs[i] = x
-                    nth = y
+                    nth = min(np.max(candidate_ys), best_metric)
     return candidate_xs[np.isfinite(candidate_ys)], candidate_ys[np.isfinite(candidate_ys)], negative_value
